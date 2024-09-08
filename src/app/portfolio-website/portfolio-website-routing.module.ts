1336116efc75aaaed5b3a0c977a92d8f807e1942
Simanga <simacoder@hotmail>
2024-09-08 22:15:02 +0200

    rerouting

--- src/app/portfolio-website/portfolio-website-routing.module.ts
+++ src/app/portfolio-website/portfolio-website-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   {path:'home', component:HomeComponent},
   {path:'about', component:AboutComponent},
   {path:'contact', component:ContactComponent},
-  { path: '**', redirectTo: '/home' } // Handle unknown routes
+  { path: '', redirectTo: '/portfolio-website', pathMatch: 'full' }// Handle unknown routes
 ];
 
 @NgModule({
