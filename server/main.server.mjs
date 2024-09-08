fd11f991bc6e34c80bf36c130b9f8e4851d2ddf9
Simanga <simacoder@hotmail>
2024-09-08 22:42:37 +0200

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-5XVZEQYS.mjs";import"./chunk-TB2TXNZQ.mjs";import"./chunk-YGPLRHBO.mjs";import"./chunk-H3B2D2HN.mjs";import"./chunk-B2QXACYC.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
